4e87ac7c90c95881bf5347a153cfda9e1061b917
waldir <waldirmendozahuaman887@gmail.com>
2025-04-08 01:33:24 -0500

    fix: component footer and header added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,8 @@
-import styles from "./page.module.css";
 
 export default function Home() {
   return (
-    <div >
-     ESTE ES EL LOGIN EN HOME
+    <div className="">
+      ESTE ES EL LOGIN EN HOME
     </div>
   );
 }
